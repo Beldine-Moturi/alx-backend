a4493a57d4291c2548a6e695ef800f9f09342318
Beldine-Moturi <beldinemoturi@gmail.com>
2022-09-23 21:19:44 +0300

    first commit

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Defines a helper function for indexing"""
+
+def index_range(page, page_size):
+    """returns start and end indexes of rows for a page"""
+
+    rows = page * page_size
+    end_index = rows
+    start_index = rows - page_size
+    return (start_index, end_index)
