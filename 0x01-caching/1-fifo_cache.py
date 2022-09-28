6a1b1eeaa76556cd8f08f25ae0a622a7781186f0
Beldine-Moturi <beldinemoturi@gmail.com>
2022-09-28 20:04:18 +0300

    FIFO caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""FIFO caching"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """defines methods that implement FIFO caching"""
+
+    def put(self, key, item):
+        """Stores an item in the cache"""
+
+        if key and item:
+            self.cache_data[key] = item
+            if len(self.cache_data) > self.MAX_ITEMS:
+                first_item = next(iter(self.cache_data))
+                self.cache_data.pop(first_item)
