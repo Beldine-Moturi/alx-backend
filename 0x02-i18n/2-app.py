d510ca7f33315a06472cc016fa1660f04d1c7ac0
Beldine-Moturi <beldinemoturi@gmail.com>
2022-10-14 05:35:24 +0300

    fix errors

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -22,8 +22,7 @@ app.config.from_object(Config)
 
 @babel.localeselector
 def get_locale():
-    """Determines the best match for the supported languages"""
-
+    """Gets the best match from supported languages."""
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
