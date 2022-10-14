d55e9c28555168f7bf750f3dd9ce00c6bb59b4bb
Beldine-Moturi <beldinemoturi@gmail.com>
2022-10-14 05:27:37 +0300

    task two

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -10,10 +10,11 @@ babel = Babel(app)
 
 class Config(object):
     """Configures available languages"""
-    
+
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 app.config.from_object(Config)
 
