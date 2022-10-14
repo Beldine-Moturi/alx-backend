c8052513a3d8f2fab5764188d12bbef04a413f5f
Beldine-Moturi <beldinemoturi@gmail.com>
2022-10-14 05:03:45 +0300

    first task

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -4,11 +4,13 @@ from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
     """Renders the hello world template"""
 
     return render_template("0-index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
